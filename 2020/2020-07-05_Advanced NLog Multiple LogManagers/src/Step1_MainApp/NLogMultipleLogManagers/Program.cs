e09ea6afc1612b9140d0f8add7f52e201a742d1f
datchung <datxchung@gmail.com>
2020-07-18 14:07:55 -0400

    Add example step1

---
+++ 2020/2020-07-05_Advanced NLog Multiple LogManagers/src/Step1_MainApp/NLogMultipleLogManagers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NLogMultipleLogManagers
+{
+    class Program
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            // Writes to fileFromMain.txt based on NLog.config
+            Logger.Info("Hello from Main");
+        }
+    }
+}
