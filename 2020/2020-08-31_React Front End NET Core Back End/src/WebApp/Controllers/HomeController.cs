726734d500a83df53c5a35c2e3566cb711613725
datchung <datxchung@gmail.com>
2020-09-06 13:14:14 -0400

    Remove unused code

--- 2020/2020-08-31_React Front End NET Core Back End/src/WebApp/Controllers/HomeController.cs
+++ 2020/2020-08-31_React Front End NET Core Back End/src/WebApp/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System.Diagnostics;
-using WebApp.Models;
 
 namespace WebApp.Controllers
 {
@@ -17,17 +15,6 @@ namespace WebApp.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
