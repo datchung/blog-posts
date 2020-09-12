a2e7a0b8ac58a77bf33bc3d21d0d2b21b5215994
datchung <datxchung@gmail.com>
2020-09-11 22:12:03 -0400

    Revert "Add routing to ui app"
    This reverts commit 0cf533e76d22a0ab9434e4cabb6e297c4bcb8e9c.

--- 2020/2020-09-07_CRUD Web App ReactJS and .NET Core/src/WebApp/FrontEndApp/src/App.js
+++ 2020/2020-09-07_CRUD Web App ReactJS and .NET Core/src/WebApp/FrontEndApp/src/App.js
@@ -1,29 +1,25 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import Layout from './components/common/Layout';
-import Home from './components/home/Home';
-import About from './components/about/About';
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Layout>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
