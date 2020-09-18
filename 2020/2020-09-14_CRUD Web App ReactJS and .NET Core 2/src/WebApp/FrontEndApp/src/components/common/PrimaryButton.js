7e7fbf10fcba3cee4d5611fa2af0534de9a97556
datchung <datxchung@gmail.com>
2020-09-17 22:23:14 -0400

    Add props to primary button

--- 2020/2020-09-14_CRUD Web App ReactJS and .NET Core 2/src/WebApp/FrontEndApp/src/components/common/PrimaryButton.js
+++ 2020/2020-09-14_CRUD Web App ReactJS and .NET Core 2/src/WebApp/FrontEndApp/src/components/common/PrimaryButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class PrimaryButton extends React.Component {
   render() {
     return (
-        <button className="button is-primary is-outlined">
+        <button className="button is-primary is-outlined" {...this.props}>
             {this.props.children}
         </button>
     );
