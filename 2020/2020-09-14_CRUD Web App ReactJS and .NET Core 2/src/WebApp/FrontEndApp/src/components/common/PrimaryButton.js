f2ab2ac36ccb88a95d9f7b616a0907331cef79b2
datchung <datxchung@gmail.com>
2020-09-18 22:56:40 -0400

    Change primary button style

--- 2020/2020-09-14_CRUD Web App ReactJS and .NET Core 2/src/WebApp/FrontEndApp/src/components/common/PrimaryButton.js
+++ 2020/2020-09-14_CRUD Web App ReactJS and .NET Core 2/src/WebApp/FrontEndApp/src/components/common/PrimaryButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class PrimaryButton extends React.Component {
   render() {
     return (
-        <button className="button is-primary is-outlined" {...this.props}>
+        <button className="button is-primary" {...this.props}>
             {this.props.children}
         </button>
     );
