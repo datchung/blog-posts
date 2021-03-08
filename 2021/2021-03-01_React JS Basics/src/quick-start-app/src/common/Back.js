59541a7f4f74ae92245f14362f59460419798120
datchung <datxchung@gmail.com>
2021-03-07 22:29:34 -0500

    Add back button

---
+++ 2021/2021-03-01_React JS Basics/src/quick-start-app/src/common/Back.js
@@ -0,0 +1,20 @@
+// Destructure history from props object
+function Back({ history }) {
+    function onBackClick() {
+      history.goBack();
+    }
+    
+    return (
+    <div className="columns">
+      <div className="column">
+        <button
+          className="button is-small"
+          onClick={() => onBackClick()}>
+          Back
+        </button>
+      </div>
+    </div>
+    );
+  }
+  
+  export default Back;
--- 2021/2021-03-01_React JS Basics/src/quick-start-app/src/restaurants/RestaurantItemPage.js
+++ 2021/2021-03-01_React JS Basics/src/quick-start-app/src/restaurants/RestaurantItemPage.js
@@ -1,10 +1,14 @@
-function RestaurantItemPage({location}) {
+import Back from '../common/Back';
+
+function RestaurantItemPage({location, history}) {
   return (
+
     <div className="box mt-5">
-        <p className="title is-3">{location.state.restaurant.name}</p>
-        <p className="subtitle is-5">Rating: {location.state.restaurant.rating}/5</p>
-        <p>{location.state.restaurant.description}</p>
-      </div>
+      <Back history={history} />
+      <p className="title is-3">{location.state.restaurant.name}</p>
+      <p className="subtitle is-5">Rating: {location.state.restaurant.rating}/5</p>
+      <p>{location.state.restaurant.description}</p>
+    </div>
   );
 }
 
