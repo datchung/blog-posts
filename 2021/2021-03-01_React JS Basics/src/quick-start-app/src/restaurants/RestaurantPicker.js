1b1ccbf35cc342cc9ae3ef97ffb1f2908af583b0
datchung <datxchung@gmail.com>
2021-03-08 23:18:22 -0500

    Update RestaurantPicker.js

--- 2021/2021-03-01_React JS Basics/src/quick-start-app/src/restaurants/RestaurantPicker.js
+++ 2021/2021-03-01_React JS Basics/src/quick-start-app/src/restaurants/RestaurantPicker.js
@@ -7,12 +7,12 @@ function RestaurantPicker({ selectedRestaurants }) {
       case 1:
         return 'Select one more restaurant to begin the random restaurant picker.';
       default:
-        return `${selectedCount} restaurants selected. Pick a restaurant now.`;
-    }    
+        return <button className="button is-small">Pick a restaurant now</button>;
+    }
   }
 
   return (
-    <article className="message is-info mt-5">
+    <article className="message is-info is-small mt-5">
       <div className="message-body">
         {getMessage(selectedRestaurants.length)}
       </div>
