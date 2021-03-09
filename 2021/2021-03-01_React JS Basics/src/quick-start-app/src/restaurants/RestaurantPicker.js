48ca914ec1ce6d7237884b231da4ce7086e40ee3
datchung <datxchung@gmail.com>
2021-03-08 23:05:56 -0500

    Update RestaurantPicker.js

--- 2021/2021-03-01_React JS Basics/src/quick-start-app/src/restaurants/RestaurantPicker.js
+++ 2021/2021-03-01_React JS Basics/src/quick-start-app/src/restaurants/RestaurantPicker.js
@@ -3,18 +3,20 @@ function RestaurantPicker({ selectedRestaurants }) {
   function getMessage(selectedCount) {
     switch(selectedCount) {
       case 0:
-        return 'Select two or more restaurants to begin the random restaurant picker!';
+        return 'Select two or more restaurants to begin the random restaurant picker.';
       case 1:
-        return 'One restaurant selected. Select another restaurant to begin the random restaurant picker!';
+        return 'Select one more restaurant to begin the random restaurant picker.';
       default:
-        return `${selectedCount} restaurants selected. Start random restaurant picker now.`;
+        return `${selectedCount} restaurants selected. Pick a restaurant now.`;
     }    
   }
 
   return (
-    <>
-      {getMessage(selectedRestaurants.length)}
-    </>
+    <article className="message is-info mt-5">
+      <div className="message-body">
+        {getMessage(selectedRestaurants.length)}
+      </div>
+    </article>
   );
 }
 
