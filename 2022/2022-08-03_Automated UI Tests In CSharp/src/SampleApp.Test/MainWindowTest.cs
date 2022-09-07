3ef915c1b96433bda6c49b3d9b530c4e76532bd1
datchung <datxchung@gmail.com>
2022-09-06 22:15:15 -0400

    Add comments to MainWindowTest

--- 2022/2022-08-03_Automated UI Tests In CSharp/src/SampleApp.Test/MainWindowTest.cs
+++ 2022/2022-08-03_Automated UI Tests In CSharp/src/SampleApp.Test/MainWindowTest.cs
@@ -8,6 +8,9 @@ namespace SampleApp.Test
     [TestClass]
     public class MainWindowTest
     {
+        /// <summary>
+        /// Added for convenience.
+        /// </summary>
         private static WindowsDriver<WindowsElement> Session => MainWindowSession.Session;
 
         [ClassInitialize]
@@ -26,6 +29,7 @@ namespace SampleApp.Test
         [TestMethod]
         public void AppWindowExists()
         {
+            // Find the app window based on the window's Title
             var element = Session.FindElementByName("MainWindow");
             Assert.IsNotNull(element);
         }
@@ -33,13 +37,15 @@ namespace SampleApp.Test
         [TestMethod]
         public void NameTextBoxExists()
         {
+            // Find the Name text box based on x:Name
             var element = Session.FindElementByAccessibilityId("NameTextBox");
             Assert.IsNotNull(element);
-
         }
+
         [TestMethod]
         public void SubmitButtonExists()
         {
+            // Find the Submit button based on x:Name
             var element = Session.FindElementByAccessibilityId("SubmitButton");
             Assert.IsNotNull(element);
         }
@@ -47,22 +53,35 @@ namespace SampleApp.Test
         [TestMethod]
         public void ClickSubmitOpensDialog()
         {
+            var name = "Homer";
+
             var nameTextBox = Session.FindElementByAccessibilityId("NameTextBox");
-            nameTextBox.SendKeys("Homer");
+            nameTextBox.SendKeys(name);
 
             var submitButton = Session.FindElementByAccessibilityId("SubmitButton");
             submitButton.Click();
 
+            // Allow time to launch the message box
             Thread.Sleep(500);
+
+            // Get the message box based on window title
             var messageBox = Session.FindElementByName("A MessageBox");
             Assert.IsNotNull(messageBox);
+
+            // Get the label based on text content
+            var messageLabel = Session.FindElementByName($"You submitted: {name}");
+            Assert.IsNotNull(messageLabel);
 
             var okButton = Session.FindElementByName("OK");
             Assert.IsNotNull(okButton);
 
+            // Dismiss the message box
             okButton.Click();
+
+            // Allow time to close the message box
             Thread.Sleep(500);
 
+            // The message box should no longer be displayed
             Assert.ThrowsException<WebDriverException>(() => 
             {
                 messageBox = Session.FindElementByName("A MessageBox");
